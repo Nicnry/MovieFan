17d4a69d1443448e83501a11fe9795b1522a34ce
HENRY Nicolas <Nicolas.HENRY@cpnv.ch>
2020-03-02 10:51:27 +0100

    Initial commit

---
+++ MovieFan/Controllers/MovieController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieFan.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MovieFan.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            moviefanContext db = new moviefanContext();
+            //List<Movies> movies = db.Movies.ToList();
+            List<Movies> movies = db.Movies.Include(m => m.Category).ToList();
+            return View(movies);
+        }
+
+        // GET: Movie/Details/5
+        public ActionResult Details(int id)
+        {
+            moviefanContext db = new moviefanContext();
+            Movies movie = db.Movies.Find(id);
+            return View(movie);
+        }
+
+        // GET: Movie/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movie/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Movie/Edit/5
+        public ActionResult Edit(int id)
+        {
+            moviefanContext db = new moviefanContext();
+            Movies movie = db.Movies.Include(m => m.Category).Include(m => m.Rating).SingleOrDefault(x => x.Id == id); ;
+            List <Categories> categories = db.Categories.ToList();
+            ViewBag.Categories = db.Categories;
+            ViewBag.RatingId = db.Ratings.ToList();
+            return View(movie);
+        }
+
+        // POST: Movie/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Movie/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Movie/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
