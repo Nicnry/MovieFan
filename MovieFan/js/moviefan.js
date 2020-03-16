9dcc0574d29f788a179cec77a1404bb2bd1640b0
Unknown <nicolas.henry@cpnv.ch>
2020-03-16 10:11:13 +0100

    End video 10

--- MovieFan/js/moviefan.js
+++ MovieFan/js/moviefan.js
@@ -1,3 +1,8 @@
-﻿$(document).ready(function () {
-    console.log("ready!");
-});
+﻿document.addEventListener('DOMContentLoaded', function () {
+
+    // Hide flashmessage
+    if (document.getElementById('flashmessage'))
+        setTimeout(function () {
+            flashmessage.classList.add('d-none')
+        }, 3000)
+})
--- MovieFan/js/showedit.js
+++ MovieFan/js/showedit.js
@@ -1,1 +1,13 @@
-﻿console.log("okok")
+﻿document.addEventListener('DOMContentLoaded', function () {
+
+    //Toggle Show/Edit movie
+    cmdEdit.addEventListener('click', function () {
+        divShow.classList.add('d-none')
+        divEdit.classList.remove('d-none')
+    })
+
+    cmdCancel.addEventListener('click', function () {
+        divShow.classList.remove('d-none')
+        divEdit.classList.add('d-none')
+    })
+})
